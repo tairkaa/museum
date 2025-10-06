c6211ddfb57f51bfbe94934cdbc66cc49e882cfd
tairkaa <tairkaaf@mail.ru>
2025-10-06 04:08:36 +0300

    Added base routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {
   Theme,
   presetGpnDark,
@@ -6,6 +7,8 @@ import {
   presetGpnDisplay,
 } from "@consta/uikit/Theme";
 import { MainPage } from "./components/main-page/main-page";
+import { AddPhotoPage } from "./components/add-photo-page/add-photo-page";
+import { ImportCsvPage } from "./components/import-csv-page/import-csv-page";
 
 const themes = {
   display: presetGpnDisplay,
@@ -15,6 +18,8 @@ const themes = {
 
 const App = () => {
   const [theme, setTheme] = useState("default");
+  const [isAdmin, setIsAdmin] = useState(false);
+  const [activeTab, setActiveTab] = useState("Процессоры");
 
   const toggleTheme = () => {
     setTheme((prev) =>
@@ -22,9 +27,59 @@ const App = () => {
     );
   };
 
+  const handleTabChange = (newTab) => {
+    setActiveTab(newTab);
+  };
+
+  const handleToggleAdmin = () => {
+    setIsAdmin(!isAdmin);
+  };
+
   return (
     <Theme className="App" preset={themes[theme]}>
-      <MainPage onToggleTheme={toggleTheme} currentTheme={theme} />
+      <Router>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <MainPage
+                onToggleTheme={toggleTheme}
+                currentTheme={theme}
+                isAdmin={isAdmin}
+                activeTab={activeTab}
+                onTabChange={handleTabChange}
+                onToggleAdmin={handleToggleAdmin}
+              />
+            }
+          />
+          <Route
+            path="/addphoto"
+            element={
+              <AddPhotoPage
+                onToggleTheme={toggleTheme}
+                currentTheme={theme}
+                isAdmin={isAdmin}
+                activeTab={activeTab}
+                onTabChange={handleTabChange}
+                onToggleAdmin={handleToggleAdmin}
+              />
+            }
+          />
+          <Route
+            path="/importcsv"
+            element={
+              <ImportCsvPage
+                onToggleTheme={toggleTheme}
+                currentTheme={theme}
+                isAdmin={isAdmin}
+                activeTab={activeTab}
+                onTabChange={handleTabChange}
+                onToggleAdmin={handleToggleAdmin}
+              />
+            }
+          />
+        </Routes>
+      </Router>
     </Theme>
   );
 };
---
+++ src/components/import-csv-page/import-csv-page.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { Layout } from "@consta/uikit/Layout";
+import { Button } from "@consta/uikit/Button";
+import { HeaderPro } from "../header/header";
+import { useNavigate } from "react-router-dom";
+import "./import-csv-page.css";
+
+export function ImportCsvPage({
+  onToggleTheme,
+  currentTheme,
+  isAdmin,
+  activeTab,
+  onTabChange,
+  onToggleAdmin,
+}) {
+  const navigate = useNavigate();
+
+  const handleGoHome = () => {
+    navigate("/");
+  };
+
+  return (
+    <Layout direction="column" className="ImportCsvPage">
+      <Layout>
+        <HeaderPro
+          isAdmin={isAdmin}
+          onToggleTheme={onToggleTheme}
+          currentTheme={currentTheme}
+          onTabChange={onTabChange}
+          activeTab={activeTab}
+          onToggleAdmin={onToggleAdmin}
+        />
+      </Layout>
+
+      <Layout className="import-csv-page__content">
+        <div className="import-csv-page__main">
+          <h1>Здесь будет импорт CSV</h1>
+        </div>
+      </Layout>
+    </Layout>
+  );
+}
